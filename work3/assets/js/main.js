9e7db9c90aecb25ed4c789bf5d033518a44d3250
absiddique7 <30343233+absiddique7@users.noreply.github.com>
2017-11-22 00:51:17 +0600

    work3

---
+++ work3/assets/js/main.js
@@ -0,0 +1,106 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        //wow.js
+        new WOW().init();
+        
+        //popup
+         $('.img-poppu').magnificPopup({
+                    type: 'image',
+                    gallery:{
+                        enabled:true
+                    }
+                });
+
+
+      /*==== Nav ====*/
+    $('.navbar-collapse a').on('click',function(){
+    	$(".navbar-collapse").collapse('hide');
+    });
+        
+            /*==== Smoothscroll ====*/    
+	$('#home a, .custom-navbar a').on('click', function(event) {
+		var $anchor = $(this);
+		  $('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top - 64
+		 }, 1000);
+		  event.preventDefault();
+	});	
+        
+         
+
+    
+	/*==== Sticky ====*/
+	$("#header").sticky({topSpacing:0});
+        
+        
+      //counter-up
+        $('.counter').counterUp({
+            delay: 10,
+            time: 3000
+        });
+        
+        
+        //testimonial
+         $('.testi-active').owlCarousel({
+            loop:true,
+            nav:false,
+            dots:true,
+            autoplay:true,
+            smartSpeed:1000,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        });
+        
+        /* magnificPopup video view */
+                $('.vedio-wrap a').magnificPopup({
+                    type: 'iframe'
+                });
+        
+       
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+        
+          //preloder
+       jQuery(".preloader").delay(3000).fadeOut("slow");
+        
+            //scrollup
+          $(window).scroll(function () { 
+                if($(this).scrollTop() > 10 ) { 
+                    $('.scrollup') .fadeIn(); 
+                } else { 
+                    $('.scrollup') .fadeOut();
+                }
+            });
+      
+        $('.scrollup').click(function () {
+                $ ("html,body").animate({
+                    scrollTop:0
+                },500);
+                return false;
+
+
+        });
+
+        
+    });
+
+
+}(jQuery));	
